5751c00f79d5292a65e817d415a09132228f7270
Luis <84397555+LuisFadini@users.noreply.github.com>
2026-08-30 13:27:43 -0300

    chore: sync branch with remote main

---
+++ src/lib/socials.ts
@@ -0,0 +1,20 @@
+export const socials = [
+	{
+		href: 'https://github.com/LuisFadini',
+		icon: 'ph:github-logo-duotone',
+		label: 'github',
+		color: 'text-black'
+	},
+	{
+		href: 'https://linkedin.com/in/luis-otavio-sperandio-fadini',
+		icon: 'ph:linkedin-logo-duotone',
+		label: 'linkedin',
+		color: 'text-blue-600'
+	},
+	{
+		href: 'https://monkeytype.com/profile/LuisFadini',
+		icon: 'ph:keyboard-duotone',
+		label: 'monkeytype',
+		color: 'text-yellow-500'
+	}
+] as const;
